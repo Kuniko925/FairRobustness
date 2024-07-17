64a8578d6476f21e400f8bf3f18a264e0256f51c
Kuniko Paxton <57262209+Kuniko925@users.noreply.github.com>
2024-07-17 21:16:44 +0100

    Add files via upload

--- src/imageprocessing.py
+++ src/imageprocessing.py
@@ -45,3 +45,8 @@ class MaskedImage:
             masked_image.save(new_filepath)
         
         print("Completed")
+
+def get_image_size(image_path):
+    with Image.open(image_path) as img:
+        width, height = img.size
+    return (height, width)
